208c62244a800d459101e344d44b2b4cbb08eeef
muhsaid121 <muhsaid121@guru.sd.belajar.id>
2023-10-06 23:34:53 +0800

    edit galeri pembelajaran

--- js/script.js
+++ js/script.js
@@ -43,3 +43,34 @@ var swiper = new Swiper(".slide-content", {
     },
   },
 });
+
+//swiper 2
+
+var swiper = new Swiper(".slide-konten", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
