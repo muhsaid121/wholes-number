fb21b2832cf311306fcbccde4724d5fefdc9e9e3
muhsaid121 <muhsaid121@guru.sd.belajar.id>
2023-10-08 23:57:33 +0800

    Menambahkan popup tujuan pembelajaran

--- js/script.js
+++ js/script.js
@@ -14,6 +14,17 @@ document.addEventListener("click", function (e) {
   }
 });
 
+//Popup
+document
+  .querySelector("#popup-open-btn")
+  .addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+  });
+document
+  .querySelector(".popup .popup-close-btn")
+  .addEventListener("click", function () {
+    document.querySelector(".popup").classList.remove("active");
+  });
 //swiper
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
